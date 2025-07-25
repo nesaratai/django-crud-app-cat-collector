f9a80d53f40b7cf67edb452b6b85881ab52582de
nesaratai <nesaratai@gmail.com>
2025-07-24 21:02:08 -0400

    connect database and make migrations and migrate and making models

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
+    path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
+    path('cats/', views.cat_index, name='cat-index'),
     path('about/', views.about, name='about'),
     path('', views.home, name='home'),
 ]
