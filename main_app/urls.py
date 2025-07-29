290f23f1c3ac11a4bed06276242632793491c6d5
nesaratai <nesaratai@gmail.com>
2025-07-29 18:47:27 -0400

    add form, delete and update cats

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views # Import views to connect routes to view functions
 
 urlpatterns = [
+    path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cat-update'),
+    path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cat-delete'),
     path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
     path('cats/', views.cat_index, name='cat-index'),
     path('about/', views.about, name='about'),
     path('', views.home, name='home'),
+    path('cats/create/', views.CatCreate.as_view(), name='cat-create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 
 # Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
-
 
 # Define the home view function
 def home(request):
@@ -36,3 +35,17 @@ def cat_index(request):
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+    # success_url = '/cats/'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    # Let's disallow the renaming of a cat by excluding the name field!
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
